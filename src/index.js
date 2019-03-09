3ba6ea13b1bcbb66cd2b99b83c992b083af685fa
ahmed elnady <dev.elnady@gmail.com>
2019-03-09 07:42:53 +0200

    change port number

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const {bootstrap} = require('./server/server')
 
 async function runApp() {
-    const app = await bootstrap(3000, 'dbHost', 'soc-shop')
+    const app = await bootstrap(4000, 'dbHost', 'soc-shop')
     // console.log(">>> app >>> ", app);
     return app; 
 }
